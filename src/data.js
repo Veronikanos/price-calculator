fad8908782f2381b3b8cc7f1fdfe92a835b18462
Veronica <vveronikanika@gmail.com>
2023-02-08 23:52:04 +0200

    feat: count prices for all providers

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const providers = [
+  {
+    name: 'backblaze.com',
+    min: 7,
+    storage: 0.005,
+    transfer: 0.01,
+  },
+  {
+    // має бути можливість переключатись між опціями HDD та SSD.
+    name: 'bunny.net',
+    max: 10,
+    storage: {
+      HDD: 0.01,
+      SSD: 0.02,
+    },
+    transfer: 0.01,
+  },
+  {
+    // має бути можливість переключатись між опціями Multi та Single.
+    name: 'scaleway.com',
+    min: 7,
+    storage: {
+      multi: 0.06,
+      single: 0.03, //Multi - 75 GB безкоштовно, потім $0.06.	Single - 75 GB безкоштовно, потім $0.03.
+    },
+    transfer: 0.02, // будь-яка опція - 75 GB безкоштовно, потім $0.02.
+  },
+  {
+    name: 'vultr.com',
+    min: 5,
+    storage: 0.01,
+    transfer: 0.01,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,14 +2,63 @@ const storageInput = document.querySelector('#storage');
 const storage = document.querySelector('#storageValue');
 const transferInput = document.querySelector('#transfer');
 const transfer = document.querySelector('#transferValue');
+const form = document.querySelector('form');
 
-const handleRangeInput = () => {
-  storage.innerText = storageInput.value;
-};
+// storageInput.addEventListener(
+//   'input',
+//   () => (storage.innerText = storageInput.value)
+// );
+// transferInput.addEventListener(
+//   'input',
+//   () => (transfer.innerText = transferInput.value)
+// );
 
-const handleTransferInput = () => {
-  transfer.innerText = transferInput.value;
-};
+form.addEventListener('input', handleInput);
 
-storageInput.addEventListener('input', handleRangeInput);
-transferInput.addEventListener('input', handleTransferInput);
+function handleInput(e) {
+  if (e.target.tagName !== 'INPUT') {
+    return;
+  }
+  if (e.target.name === 'storage') {
+    storage.innerText = storageInput.value;
+  } else {
+    transfer.innerText = transferInput.value;
+  }
+  let res = calculateCost(
+    parseInt(storage.innerText),
+    parseInt(transfer.innerText)
+  );
+  console.log(res);
+  // console.log(parseInt(storage.innerText));
+  // console.log(parseInt(transfer.innerText));
+
+  function calculateCost(storage, transfer) {
+    const backblazePrice = getFactPrice(storage, transfer, 0.005, 0.01);
+    const backblazeCost = backblazePrice <= 7 ? 7 : backblazePrice;
+
+    const bunnyPrice = getFactPrice(storage, transfer, 0.01, 0.01); //if hhd
+    const bunnyCost = bunnyPrice < 10 ? bunnyPrice : 10;
+
+    const scalewayfreeStorage = storage <= 75 ? 0 : storage - 75; //if multi
+    const scalewayfreeTransfer = transfer <= 75 ? 0 : transfer - 75; //if multi
+    const scalewayCost = getFactPrice(
+      scalewayfreeStorage,
+      scalewayfreeTransfer,
+      0.06,
+      0.02
+    );
+
+    const vultrPrice = getFactPrice(storage, transfer, 0.01, 0.01);
+    const vultrCost = vultrPrice <= 5 ? 5 : vultrPrice;
+
+    const priceRange = [backblazeCost, bunnyCost, scalewayCost, vultrCost];
+    // let minPrice = Math.min(...priceRange);
+    // let color = priceRange[0] === minPrice ? 'red' : 'default';
+
+    return priceRange;
+  }
+
+  function getFactPrice(storage, transfer, sValue, tValue) {
+    return +(storage * sValue + transfer * tValue).toFixed(2);
+  }
+}
