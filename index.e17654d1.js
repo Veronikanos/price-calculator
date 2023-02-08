a33217940d1cfe3ec9d2198fc51f6434debffcd2
Veronikanos <49239848+Veronikanos@users.noreply.github.com>
2023-02-08 16:58:52 +0000

    Deploying to gh-pages from @ Veronikanos/price-calculator@bf645c0579e38a430595989938946d0b543210b5 🚀

---
+++ index.e17654d1.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#storage"),t=document.querySelector("#storageValue"),n=document.querySelector("#transfer"),r=document.querySelector("#transferValue");e.addEventListener("input",(function(){t.innerText=e.value})),n.addEventListener("input",(function(){r.innerText=n.value}))}();
+//# sourceMappingURL=index.e17654d1.js.map
