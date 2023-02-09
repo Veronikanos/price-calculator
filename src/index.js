86ece5b8953023a509ee6c151342c9edb90bdf13
Veronica <vveronikanika@gmail.com>
2023-02-09 17:24:57 +0200

    feat: count all prices nd rerender when radio button switched

--- src/index.js
+++ src/index.js
@@ -4,29 +4,73 @@ const transferInput = document.querySelector('#transfer');
 const transfer = document.querySelector('#transferValue');
 const form = document.querySelector('form');
 
-const bunnyRadioButtons = document.querySelectorAll('input[name="bunny"]');
+const bunnyRadioButtons = document.querySelectorAll(
+  'input[type=radio][name="bunny"]'
+);
 const scalewayRadioButtons = document.querySelectorAll(
-  'input[name="scaleway"]'
+  'input[type=radio][name="scaleway"]'
 );
 
-// storageInput.addEventListener(
-//   'input',
-//   () => (storage.innerText = storageInput.value)
-// );
-// transferInput.addEventListener(
-//   'input',
-//   () => (transfer.innerText = transferInput.value)
-// );
+console.log(bunnyRadioButtons);
 
-form.addEventListener('input', handleInput);
+const getFactPrice = (storage, transfer, sValue, tValue) => {
+  return +(storage * sValue + transfer * tValue).toFixed(2);
+};
 
-function handleInput(e) {
+const getBackblazePrice = (storage, transfer) => {
+  const backblazePrice = getFactPrice(storage, transfer, 0.005, 0.01);
+  return backblazePrice <= 7 ? 7 : backblazePrice;
+};
+
+const getBunnyPrice = (storage, transfer) => {
+  let dataStorageDevice = document.querySelector(
+    'input[name="bunny"]:checked'
+  ).value;
+  const storagePrice = dataStorageDevice === 'hdd' ? 0.01 : 0.02;
+  const bunnyPrice = getFactPrice(storage, transfer, storagePrice, 0.01);
+  return bunnyPrice < 10 ? bunnyPrice : 10;
+};
+
+const getScalewayPrice = (storage, transfer) => {
+  let dataStorageUsers = document.querySelector(
+    'input[name="scaleway"]:checked'
+  ).value;
+  const storagePrice = dataStorageUsers === 'multi' ? 0.06 : 0.03;
+  const scalewayfreeStorage = storage <= 75 ? 0 : storage - 75;
+  const scalewayfreeTransfer = transfer <= 75 ? 0 : transfer - 75;
+  return getFactPrice(
+    scalewayfreeStorage,
+    scalewayfreeTransfer,
+    storagePrice,
+    0.02
+  );
+};
+
+const getVultrPrice = (storage, transfer) => {
+  const vultrPrice = getFactPrice(storage, transfer, 0.01, 0.01);
+  return vultrPrice <= 5 ? 5 : vultrPrice;
+};
+
+const calculateCost = (storage, transfer) => {
+  const backblazeCost = getBackblazePrice(storage, transfer);
+  const bunnyCost = getBunnyPrice(storage, transfer);
+  const scalewayCost = getScalewayPrice(storage, transfer);
+  const vultrCost = getVultrPrice(storage, transfer);
+
+  const priceRange = [backblazeCost, bunnyCost, scalewayCost, vultrCost];
+  // let minPrice = Math.min(...priceRange);
+  // let color = priceRange[0] === minPrice ? 'red' : 'default';
+
+  return priceRange;
+};
+
+const handleInput = e => {
   if (e.target.tagName !== 'INPUT') {
     return;
   }
   if (e.target.name === 'storage') {
     storage.innerText = storageInput.value;
-  } else {
+  } else if (e.target.name === 'transfer') {
     transfer.innerText = transferInput.value;
   }
   let res = calculateCost(
@@ -34,60 +78,16 @@ function handleInput(e) {
     parseInt(transfer.innerText)
   );
   console.log(res);
+};
 
-  // console.log(parseInt(storage.innerText));
-  // console.log(parseInt(transfer.innerText));
+// Listeners
 
-  function getBackblazePrice(storage, transfer) {
-    const backblazePrice = getFactPrice(storage, transfer, 0.005, 0.01);
-    return backblazePrice <= 7 ? 7 : backblazePrice;
-  }
+form.addEventListener('input', handleInput);
 
-  function getBunnyPrice(storage, transfer) {
-    const bunnyRadioButtons = document.querySelectorAll('input[name="bunny"]');
-    let dataStorageDevice = 'hdd';
-    for (let i of bunnyRadioButtons) {
-      if (i.checked) {
-        dataStorageDevice = i.value;
-        break;
-      }
-    }
-    const storagePrice = dataStorageDevice === 'hdd' ? 0.01 : 0.02;
-    const bunnyPrice = getFactPrice(storage, transfer, storagePrice, 0.01);
-    return bunnyPrice < 10 ? bunnyPrice : 10;
-  }
+bunnyRadioButtons.forEach(radio =>
+  radio.addEventListener('change', handleInput)
+);
 
-  function calculateCost(storage, transfer) {
-    const backblazeCost = getBackblazePrice(storage, transfer);
-    const bunnyCost = getBunnyPrice(storage, transfer);
-
-    const scalewayfreeStorage = storage <= 75 ? 0 : storage - 75; //if multi
-    const scalewayfreeTransfer = transfer <= 75 ? 0 : transfer - 75; //if multi
-    const scalewayCost = getFactPrice(
-      scalewayfreeStorage,
-      scalewayfreeTransfer,
-      0.06,
-      0.02
-    );
-
-    const vultrPrice = getFactPrice(storage, transfer, 0.01, 0.01);
-    const vultrCost = vultrPrice <= 5 ? 5 : vultrPrice;
-
-    const priceRange = [backblazeCost, bunnyCost, scalewayCost, vultrCost];
-    // let minPrice = Math.min(...priceRange);
-    // let color = priceRange[0] === minPrice ? 'red' : 'default';
-
-    return priceRange;
-  }
-
-  function getFactPrice(storage, transfer, sValue, tValue) {
-    return +(storage * sValue + transfer * tValue).toFixed(2);
-  }
-}
-
-// const fruits = document.querySelectorAll('input[name="fruit"]')
-// for (const f of fruits) {
-//   if (f.checked) {
-//     console.log(f.value)
-//   }
-// }
+scalewayRadioButtons.forEach(radio =>
+  radio.addEventListener('change', handleInput)
+);
